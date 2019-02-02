8bdd09b2a3fbf86a79d2b653e20d3086636d8603
Smoore090 <spencerhmoore090@gmail.com>
2019-02-02 13:29:52 -0500

    error

--- app.js
+++ app.js
@@ -11,15 +11,154 @@ $(document).ready(function() {
 
     // Initialize Firebase
     var config = {
-        apiKey: "AIzaSyB7FoClUg_vFWCfyZLegDveIwcnbr3WIcs",
-        authDomain: "stocks4hire-488ef.firebaseapp.com",
-        databaseURL: "https://stocks4hire-488ef.firebaseio.com",
-        projectId: "stocks4hire-488ef",
-        storageBucket: "stocks4hire-488ef.appspot.com",
-        messagingSenderId: "233038305491"
-    };
-    firebase.initializeApp(config);
+        apiKey: "AIzaSyDfQP6TQSTiLKBpE16fqOd_JDx-xfCS55g",
+        authDomain: "stocks-eeae4.firebaseapp.com",
+        databaseURL: "https://stocks-eeae4.firebaseio.com",
+        projectId: "stocks-eeae4",
+        storageBucket: "stocks-eeae4.appspot.com",
+        messagingSenderId: "623424739833"
+      };
+      firebase.initializeApp(config);
     
+    let database = firebase.database();
+    let auth = firebase.auth()
+
+    // $("#signIn").on("click", function(event) {
+//     event.preventDefault()
+//     const email = $("#email").val().trim()
+//     const pass = $("#password").val().trim()
+//     const signIn = auth.signInWithEmailAndPassword(email,pass)
+//     signIn.catch(e => console.log(e.message))
+// })
+    console.log('running')
+$("#signUp").on("click", function(event) {
+    console.log("test")
+    event.preventDefault();
+    email = $("#email").val().trim()
+    pass = $("#password").val().trim()
+    console.log("email")
+    const signUp = auth.createUserWithEmailAndPassword(email,pass)
+    signUp.catch(e => console.log(e.message))
+})
+
+auth.onAuthStateChanged(firebaseUser => {
+    if (firebaseUser) {
+        console.log(`user logged in`)
+    } else {
+        console.log(`user logged out`)
+    }
+})
+
+
+let API = "https://api.iextrading.com/1.0"
+let query = "/stock/aapl/chart"
+let date = 5
+// let yAxesMin;
+// let yAxesMax;
+let stockValue = []
+function getURL() {
+url = API + query
+$.get(url).then(function(obj) {
+
+    console.log(obj);
+
+    for (let i = 0; i < 20; i++) {
+    date.push(obj[i].date)
+    stockValue.push(obj[i].vwap)
+    // yAxesMin = Math.min(...stockValue) - 10
+    console.log(stockValue)
+    }
+}).catch( error => console.log(error))
+} 
+getURL()
+// chart need to make it so that it changes based on different click functions
+setTimeout(function() {
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: date,
+        datasets: [{
+            label: 'stock price',
+            data: stockValue,
+            backgroundColor: [
+                'rgba(0, 0, 0, 0.5)'
+            ],
+            borderColor: [
+                'rgba(0, 255, 0, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive:true,
+        maintainAspectRatio:true,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:false
+                }
+            }]
+        }
+    }
+            });
+        }, 1);
+
+        
+            var click = $(this).attr("val");
+
+            $("#" + click).hide();
+
+            console.log(click);
+        });
+
+        console.log("Made chart");
+
+    $(document).on("click", ".chart", function(){
+
+        console.log("hey")
+        
+        var click = $(this).attr("val");
+
+        if(!clicked){
+
+            $("#" + click).show();
+
+            console.log(clicked);
+
+            clicked = true;
+        }
+        else if(clicked){
+
+            $("#" + click).hide();
+            
+            clicked = false;
+        }
+    });
+
+function getNews(item){
+
+    var URL = 'https://newsapi.org/v2/everything?q=' + item + 's&apiKey=d53b18e6f2bb4408bb4b79dd3dfb406b'
+
+    $.ajax({
+        url: URL,
+        method: "GET"
+      })
+      .then(function(response){
+        
+        for(i = 0; i < 5; i++){
+            var newsURL = response.articles[i].url;
+            var title = response.articles[i].title;
+            var date = response.articles[i].publishedAt.substr(0,10);
+            var author = response.articles[i].author;
+            
+            $("#news" + i).append('<a href="'+newsURL+'" target="blank">'+title+' (Date: '+date+') '+'Author: '+author+'</a><br>');
+        
+      };
+    });
+}
+
+});
 
     $("#add-button").on("click", function(event){
         event.preventDefault();
@@ -117,154 +256,8 @@ $(document).ready(function() {
                             beginAtZero:false
                         }
                     }]
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDfQP6TQSTiLKBpE16fqOd_JDx-xfCS55g",
-    authDomain: "stocks-eeae4.firebaseapp.com",
-    databaseURL: "https://stocks-eeae4.firebaseio.com",
-    projectId: "stocks-eeae4",
-    storageBucket: "stocks-eeae4.appspot.com",
-    messagingSenderId: "623424739833"
-  };
-  firebase.initializeApp(config);
-
-let database = firebase.database();
-let auth = firebase.auth()
-// $("#signIn").on("click", function(event) {
-//     event.preventDefault()
-//     const email = $("#email").val().trim()
-//     const pass = $("#password").val().trim()
-//     const signIn = auth.signInWithEmailAndPassword(email,pass)
-//     signIn.catch(e => console.log(e.message))
-// })
-console.log('running')
-$("#signUp").on("click", function(event) {
-    console.log("test")
-    event.preventDefault();
-    email = $("#email").val().trim()
-    pass = $("#password").val().trim()
-    console.log("email")
-    const signUp = auth.createUserWithEmailAndPassword(email,pass)
-    signUp.catch(e => console.log(e.message))
-})
-
-auth.onAuthStateChanged(firebaseUser => {
-    if (firebaseUser) {
-        console.log(`user logged in`)
-    } else {
-        console.log(`user logged out`)
-    }
-})
 
 
-let API = "https://api.iextrading.com/1.0"
-let query = "/stock/aapl/chart"
-let date = 5
-// let yAxesMin;
-// let yAxesMax;
-let stockValue = []
-function getURL() {
-url = API + query
-$.get(url).then(function(obj) {
-
-    console.log(obj);
-
-    for (let i = 0; i < 20; i++) {
-    date.push(obj[i].date)
-    stockValue.push(obj[i].vwap)
-    // yAxesMin = Math.min(...stockValue) - 10
-    console.log(stockValue)
-    }
-}).catch( error => console.log(error))
-} 
-getURL()
-// chart need to make it so that it changes based on different click functions
-setTimeout(function() {
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: date,
-        datasets: [{
-            label: 'stock price',
-            data: stockValue,
-            backgroundColor: [
-                'rgba(0, 0, 0, 0.5)'
-            ],
-            borderColor: [
-                'rgba(0, 255, 0, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        responsive:true,
-        maintainAspectRatio:true,
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:false
-                }
-            }
-    
-            });
-        }, 1);
-
-        
-            var click = $(this).attr("val");
-
-            $("#" + click).hide();
-
-            console.log(click);
-        });
-
-        console.log("Made chart");
-
-    $(document).on("click", ".chart", function(){
-
-        console.log("hey")
-        
-        var click = $(this).attr("val");
-
-        if(!clicked){
-
-            $("#" + click).show();
-
-            console.log(clicked);
-
-            clicked = true;
-        }
-        else if(clicked){
-
-            $("#" + click).hide();
-            
-            clicked = false;
-        }
-    });
-
-function getNews(item){
-
-    var URL = 'https://newsapi.org/v2/everything?q=' + item + 's&apiKey=d53b18e6f2bb4408bb4b79dd3dfb406b'
-
-    $.ajax({
-        url: URL,
-        method: "GET"
-      })
-      .then(function(response){
-        
-        for(i = 0; i < 5; i++){
-            var newsURL = response.articles[i].url;
-            var title = response.articles[i].title;
-            var date = response.articles[i].publishedAt.substr(0,10);
-            var author = response.articles[i].author;
-            
-            $("#news" + i).append('<a href="'+newsURL+'" target="blank">'+title+' (Date: '+date+') '+'Author: '+author+'</a><br>');
-        
-      };
-    });
-}
-
-});
 // when you click on a stock it dropsdown below(using bootstrap) and shows a graph with one month of stock data
 
 // when clicking the star icon next to the stock it will add it to the users favorites firebase
